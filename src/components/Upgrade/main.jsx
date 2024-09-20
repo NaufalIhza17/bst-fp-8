3adb4b7319c1aa849b8c8ded27e9618605d0bdb5
NaufalIhza17 <nawfal17syahzada@gmail.com>
2024-09-20 16:24:54 +0800

    fix: responsive versatile & upgrade

--- src/components/Upgrade/main.jsx
+++ src/components/Upgrade/main.jsx
@@ -8,16 +8,16 @@ export const upgradeData = [
 
 export default function UpgradeComponents() {
   return (
-    <section className="w-full bg-white bg-review bg-center px-5 py-10">
-      <div className="layout bg-upgrade flex items-center justify-around rounded-2xl">
-        <h2 className="text-white font-semibold text-5xl max-w-[544px]">
+    <section className="w-full bg-white bg-review bg-center px-3 sm:px-5 py-10">
+      <div className="max-w-[1440px] w-full mx-auto bg-upgrade flex flex-col sm:flex-row items-start md:items-center justify-around rounded-2xl px-5 gap-10 py-20 lg:py-[140px]">
+        <h2 className="text-white font-semibold text-4xl sm:text-5xl max-w-[544px]">
           Upgrade your bathroom for a fresh, stylish vibe.
         </h2>
-        <div className="flex flex-col gap-6 my-[140px]">
+        <div className="grid max-sm:grid-cols-2 gap-4 md:gap-6 max-sm:w-full">
           {upgradeData.map((data) => (
             <div
               key={data}
-              className="w-[309px] py-4 bg-white/80 rounded-full text-center hover:scale-110 group hover:bg-[#8A95A6] transition-all"
+              className="w-[309px] max-sm:w-full max-md:w-[260px] py-4 bg-white/80 rounded-full text-center hover:scale-110 group hover:bg-[#8A95A6] transition-all"
             >
               <p className="font-medium text-[#8E8E8E] group-hover:text-white">
                 {data}
--- src/components/Versatile/main.jsx
+++ src/components/Versatile/main.jsx
@@ -1,12 +1,12 @@
 export default function VersatileComponents() {
   return (
-    <section className="w-full bg-white bg-review bg-center py-[140px] px-20">
-      <div className="layout grid gap-[72px]">
-        <div className="flex justify-between gap-10">
+    <section className="w-full bg-white bg-review bg-center py-14 lg:py-20 px-10 lg:px-20">
+      <div className="max-w-[1440px] w-full mx-auto grid gap-9 lg:gap-[72px]">
+        <div className="flex flex-col lg:flex-row justify-between gap-4 lg:gap-10">
           <h1 className="font-semibold text-[#101010] text-2xl min-w-max">
             <span className="text-[#FF8A00]">/ </span>Versatile Opening Options
           </h1>
-          <p className="font-medium text-[28px] text-[#101010] max-w-[685px]">
+          <p className="font-medium text-xl lg:text-[28px] text-[#101010] lg:max-w-[685px]">
             Indulge in the convenience of a myriad of versatile opening options,{" "}
             <span className="text-[#667085]">
               providing you with a customizable and adaptable experience
@@ -15,7 +15,7 @@ export default function VersatileComponents() {
           </p>
         </div>
         <div
-          className="text-[#101010] flex gap-8 justify-between text-lg uppercase"
+          className="text-[#101010] flex max-sm:flex-wrap gap-8 justify-between text-base lg:text-lg uppercase"
         >
           <div className="flex flex-col gap-4 max-w-[348px]">
             <img src={"/images/step-1.jpg"} alt="" width={348} height={461} />
